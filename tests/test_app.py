2a6490238188fa606986bfa19fb4e6a66c6826db
pratyushdingh8 <pratyush1275@gmail.com>
2025-10-13 00:32:50 +0530

    test: add pytest and basic homepage test

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+# tests/test_app.py
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_homepage(client):
+    response = client.get('/')
+    assert response.status_code == 200
